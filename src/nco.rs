54f49fc22b51b3d8cf1e13827e4d5ffa6c676bec
@zip <zip@keybase.io>
2016-05-26 21:05:40 -0700

    name sample_rate properly

--- src/nco.rs
+++ src/nco.rs
@@ -6,7 +6,7 @@ use signal::Signal;
 pub struct NCOTable {
     pub bits: usize,
     pub fractional: usize,
-    pub fundamental: f32,
+    pub sample_rate: f32,
     samples: Vec<f32>,
 }
 
@@ -19,20 +19,20 @@ struct NCO<'a> {
 }
 
 impl<'a> NCOTable {
-    // fundamental - the sample rate, effectively. eg 44100Hz.
+    // sample_rate - the sample rate, effectively. eg 44100Hz.
     // bits - resolution of the lookup table
     // fractional - allow for tinier steps
-    // In short this works by using integer fractions of the fundamental
-    // eg if your fundamental is 44100Hz and your bits are 8, you can
+    // In short this works by using integer fractions of the sample_rate
+    // eg if your sample_rate is 44100Hz and your bits are 8, you can
     // only oscillate at 1, 1/2, 1/3, 1/4 of the (1<<8) entries.
     // fractional resolution lets you get closer to other values.
     #[allow(dead_code)]
-    pub fn new(fundamental: f32, bits: usize, fractional: usize) -> NCOTable {
+    pub fn new(sample_rate: f32, bits: usize, fractional: usize) -> NCOTable {
         let max = 1 << bits;
         NCOTable {
             bits: bits,
             fractional: fractional,
-            fundamental: fundamental,
+            sample_rate: sample_rate,
             samples: (0 .. max).map(|s| {
                 ((s as f32 / max as f32) * 2.0 * PI).sin()
             }).collect(),
@@ -40,7 +40,7 @@ impl<'a> NCOTable {
     }
 
     pub fn steps_for_freq(&self, freq: f32) -> usize {
-        ((1 << (self.bits + self.fractional)) as f32 * (freq / self.fundamental)).floor() as usize
+        ((1 << (self.bits + self.fractional)) as f32 * (freq / self.sample_rate)).floor() as usize
     }
 
     #[allow(dead_code)]
